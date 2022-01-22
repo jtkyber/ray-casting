9eacd11832f5b3cbeb699439afd8de6ce5239973
jtkyber <43868043+jtkyber@users.noreply.github.com>
2022-01-22 12:43:15 -0500

    removed test server

--- server.js
+++
@@ -1,6 +0,0 @@
-// const express = require('express');
-// const app = express();
-// const port = process.env.PORT || 3000;
-
-// app.listen(port, () => console.log('listening at 3000'));
-// app.use(express.static('public'));
